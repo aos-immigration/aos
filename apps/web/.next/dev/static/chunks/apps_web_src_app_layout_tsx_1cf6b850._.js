e7bb7b32339a0c4e58382d1de2383fc23d9c5380
Tawsif Kamal <tawsifkamal123@gmail.com>
2026-01-25 15:36:58 -0800

    chore: Remove build artifacts and cache files from git tracking
    - Remove .next build files from git tracking
    - Remove __pycache__ files from git tracking
    - Remove .turbo cache files from git tracking
    - Update .gitignore to prevent future commits of these files

--- apps/web/.next/dev/static/chunks/apps_web_src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__5bf117b1._.css",
-  "static/chunks/node_modules_977b2c4e._.js",
-  "static/chunks/apps_web_src_app_providers_tsx_a1932fcc._.js"
-],
-    source: "dynamic"
-});
